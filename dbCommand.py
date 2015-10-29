bc4c995c0bf80a8d456785b6848129c0dc1e3a18
Darthyogurt <raytochina@gmail.com>
2015-10-28 21:48:45 -0700

    fixed the adding user bug

--- dbCommand.py
+++ dbCommand.py
@@ -1,5 +1,52 @@
 from pymongo import MongoClient
+from datetime import datetime
+# from mongothon import Schema
+import mongothon
+
+import config
+
+client = MongoClient(config.MONGO_URL)
+# dbNews = client['lovel-up']
+db = client['twitter_bot']
+
+user_schema = mongothon.Schema({
+    "id":               {"type": basestring, "required": True},
+    "name":             {"type": basestring},
+    "date_followed":    {"type": datetime },
+    "date_unfollowed":  {"type": datetime },
+    "following":        {"type": bool},
+    "following_me":     {"type": bool}
+})
 
 
-def get_users():
-    return "DATABASE"
+User = mongothon.create_model(user_schema, db['users'])
+
+def get_user_by_id(id):
+    print id
+    return User.find_one({'id': str(id)})
+
+
+def add_user(user):
+
+    newUser = User({
+                "id": str(user['id']),
+                "name": user['name'],
+                "following": True
+            })
+    newUser.save()
+
+def add_user_id(id):
+    newUser = User({
+        "id": str(id),
+        "following": False,
+        "following_me": True
+    })
+    newUser.save()
+
+def update_following_me_true(id):
+    User.update({"id": id}, {"following_me": {"$set": True}})
+
+def update_following_me(user):
+    usr =  User.find_one({'id': user['id_str']})
+    usr['following_me'] = False
+    usr.save()
